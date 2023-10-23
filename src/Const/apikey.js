2e4c3bf380726af7dc7b897b9d91e8c2e0f6193e
uniend <dudnsgldns@naver.com>
2023-10-23 23:56:35 +0900

    '공통스타일과키'

---
+++ src/Const/apikey.js
@@ -0,0 +1,21 @@
+const API_KEY = {
+  HOME :'/home',
+  ERROR :'/error',
+  LANDING : '/landing',
+  LOGIN : '/login',
+  LOGOUT : '/logout',
+  SIGNIN : '/signin',
+  LIST : '/list',
+  MYPAGE: '/mypage',
+  ACCOUNT : '/account',
+  PAYLIST : '/paylist',
+  CART : '/cart',
+  ABOUT : '/about',
+  SEARCH : '/search',
+  QA: '/qa',
+  UPLOAD : '/upload',
+  DETAIL : '/detail',
+  CATAGORY: '/catagory'
+}
+
+export default API_KEY
---
+++ src/Styles/common.js
@@ -0,0 +1,18 @@
+import {css} from 'styled-components'
+
+export const flexCenter = css`
+  display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+export const flexAlignCenter = css`
+	display: flex;
+	align-items: center;
+`
+
+export const flexBetween = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
